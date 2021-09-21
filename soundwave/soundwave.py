68ccb9293a79e6a3eca3062b4d893e018a780877
GitAcrown <Acrown@outlook.fr>
2021-09-21 03:52:43 +0200

    ajout sécurité filzsize

--- soundwave/soundwave.py
+++ soundwave/soundwave.py
@@ -42,14 +42,14 @@ class Soundwave(commands.Cog):
     async def download_attachment(self, msg: discord.Message):
         path = str(self.temp)
         seed = str(int(time.time()))
-        if msg.attachments[0].size <= 1e7:
+        if msg.attachments[0].size <= 2e6:
             filename = "{}_{}".format(seed, msg.attachments[0].filename)
             filepath = "{}/{}".format(str(path), filename)
             await msg.attachments[0].save(filepath)
             return filepath
         else:
             return None
-    
+    a
     async def audio_to_video(self, audio_path: str, image_path: str, output_path: str):
         com = ['ffmpeg', '-loop', '1', '-i', f'{image_path}', '-i', f'{audio_path}', '-c:v', 'libx264', '-tune', 'stillimage', '-c:a', 'aac', '-b:a', '192k', '-pix_fmt', 'yuv420p', '-shortest', f'{output_path}.mp4']
         pr = subprocess.Popen(com, stdout=subprocess.PIPE, stderr=subprocess.PIPE)

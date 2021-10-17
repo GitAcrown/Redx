4aab954694e668acc37820ddf450e3d116d4c737
GitAcrown <Acrown@outlook.fr>
2021-10-17 21:51:47 +0200

    ajout

--- oktbr/oktbr.py
+++ oktbr/oktbr.py
@@ -607,7 +607,7 @@ class Oktbr(commands.Cog):
         
         if cache['SickUser'].get(author.id, 0) + 3600 > time.time():
             new = (cache['SickUser'][author.id] + 3600) - time.time()
-            return await ctx.reply(f"{cross} **Maladie** · Vous êtes malade. Vous ne pourrez pas reconsommer d'item avant *{new}*.",
+            return await ctx.reply(f"{cross} **Maladie** · Vous êtes malade. Vous ne pourrez pas reconsommer d'item avant *{humanize_timedelta(new)}*.",
                                    mention_author=False)
         
         if not item:
@@ -747,7 +747,7 @@ class Oktbr(commands.Cog):
         cache = self.get_cache(ctx.guild)
         if cache['SickUser'].get(author.id, 0) + 3600 > time.time():
             new = (cache['SickUser'][author.id] + 3600) - time.time()
-            return await ctx.reply(f"{cross} **Maladie** · Vous êtes malade. Vous ne pouvez pas réaliser cette action avant *{new}*.",
+            return await ctx.reply(f"{cross} **Maladie** · Vous êtes malade. Vous ne pouvez pas réaliser cette action avant *{humanize_timedelta(new)}*.",
                                    mention_author=False)
         
         current = await self.config.member(author).Sugar()

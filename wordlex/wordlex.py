8642ccae75aa0ff7a3929bab99bfe08cd98dbe3f
GitAcrown <Acrown@outlook.fr>
2022-03-02 03:26:45 +0100

    ajout du versus (alpha)

--- wordlex/wordlex.py
+++ wordlex/wordlex.py
@@ -6,6 +6,7 @@ import time
 from datetime import datetime
 from operator import itemgetter
 from copy import copy
+from io import StringIO
 
 import discord
 from typing import Callable, List, Union
@@ -59,7 +60,7 @@ class WordleX(commands.Cog):
         
         return lscore
 
-    def score_words(self, lang: str, wordlist: list = None, first: bool = False) -> dict:
+    def score_words(self, lang: str, wordlist: list = None, first: bool = False, letter_balance: dict = None) -> dict:
         """Calcule un score des mots disponibles en fonction de l'apparition des lettres dans la langue séléctionnée"""
         if not wordlist:
             try:
@@ -68,6 +69,10 @@ class WordleX(commands.Cog):
                 raise KeyError(f"La langue '{lang}' n'est pas disponible")
         
         lscore = self.most_used_letters(lang)
+        if letter_balance:
+            for l in letter_balance:
+                lscore[l] = round(lscore[l] * (0.80 ** letter_balance[l]))
+            
         wscore = {}
         for w in wordlist:
             wscore[w] = sum([lscore[l] for l in list(set(w))]) if first else sum([lscore[l] for l in w])
@@ -112,15 +117,18 @@ class WordleX(commands.Cog):
                         wordlist.remove(w)
                         break
         
-        lower_letters = [l for wl in tries for l in wl if l.islower()]
+        lower_letters = set([l for wl in tries for l in wl if l.islower()])
         for ll in lower_letters:
             wlcache = copy(wordlist)
             for w in wlcache:
                 if ll not in w:
                     wordlist.remove(w)
                     continue
+        
+        alltries = [l for w in tries for l in w]
+        llcount = {s: alltries.count(s) for s in set(alltries)}
                     
-        return self.score_words(lang, wordlist)
+        return self.score_words(lang, wordlist, letter_balance=llcount)
     
 
     @commands.group(name="wordle")
@@ -191,7 +199,7 @@ class WordleX(commands.Cog):
         else:
             word = mot.lower()
         
-        em = discord.Embed(title=f"**Wordle** · Résultat de #{wnum}", description=f"Indiquez le résultat de cette tentative : **{word.upper()}**" + "\n\n" + f"`{word[0].lower()}` Lettre minuscule si lettre en mauvaise position (orange)" + "\n"+ f"`{word[0].upper()}` Lettre majuscule si lettre en bonne position (vert)" + "\n" + "`-` Si mauvaise lettre (gris)")
+        em = discord.Embed(title=f"**Wordle** · Résultat de #{wnum}", description=f"Indiquez le résultat de cette tentative : **{word.upper()}**" + "\n\n" + f"`{word[0].lower()}` Lettre minuscule si lettre en mauvaise position (orange)" + "\n"+ f"`{word[0].upper()}` Lettre majuscule si lettre en bonne position (vert)" + "\n" + "`- ou .` Si mauvaise lettre (gris)")
         em.set_footer(text="» Indiquez ci-dessous le résultat :")
             
         msg = await ctx.reply(embed=em, mention_author=False)
@@ -203,6 +211,7 @@ class WordleX(commands.Cog):
             return await ctx.reply(f"**Session expirée** · Relancez la commande quand vous voudrez soumettre vos choix et résultats Wordle", mention_author=False)
         
         result = resultrep.content
+        result = result.replace('.', '-')
         
         for x, y in zip(result, word):
             if x == '-':
@@ -257,3 +266,132 @@ class WordleX(commands.Cog):
             em.set_footer(text="Utilisez 'wordle start' pour retenter la prochaine fois !")
             await self.config.user(user).Solver.clear()
             return await ctx.reply(embed=em, mention_author=False)
+        
+
+    @wordle_solver.command(name="versus")
+    @commands.max_concurrency(1, commands.BucketType.user)
+    async def play_vs_bot(self, ctx):
+        """Jouez contre le bot : il tente de deviner le mot que vous avez en tête (EN)"""
+        user = ctx.author
+        lang = 'en'
+        
+        check, cross = self.bot.get_emoji(812451214037221439), self.bot.get_emoji(812451214179434551)
+        em = discord.Embed(description="Je ne peux que deviner les mots des listes utilisées par Wordle. Voulez-vous que je vous envoie cette liste ?")
+        em.set_footer(text="Recevoir | Passer")
+        
+        msg = await ctx.reply(embed=em, mention_author=False)
+        start_adding_reactions(msg, [check, cross])
+        try:
+            react, _ = await self.bot.wait_for("reaction_add", check=lambda m, u: u == ctx.author and m.message.id == msg.id, timeout=30)
+        except asyncio.TimeoutError:
+            await msg.delete()
+        if react.emoji == cross:
+            await msg.delete(delay=2)
+        elif react.emoji == check:
+            text = '\n'.join(self.words['en'])
+            f = StringIO(text)
+            await msg.reply(mention_author=False, file=discord.File(f, 'Liste_mots_EN.txt'))
+            await msg.delete(delay=10)
+        
+        em = discord.Embed(description="**__Règles :__**\nJe dois deviner le mot de 5 lettres que vous avez choisi (en anglais) en **6 tentatives**, sinon j'ai perdu. Je compte sur vous pour ne pas tricher et changer de mot en cours de chemin... Sinon...\n" + f"Bref, quand vous aurez choisi votre mot cliquez sur {check} ci-dessous pour démarrer la partie !")
+        em.set_footer(text="Cliquez ci-dessous pour continuer (5m)")
+        msg = await ctx.reply(embed=em, mention_author=False)
+        
+        start_adding_reactions(msg, [check])
+        try:
+            react, _ = await self.bot.wait_for("reaction_add", check=lambda m, u: u == ctx.author and m.message.id == msg.id, timeout=300)
+        except asyncio.TimeoutError:
+            return await ctx.send("**Partie annulée** · Vous avez eu peur de perdre ? Je peux comprendre. A la prochaine !")
+        
+        propcount = 0
+        tries = []
+        wrong = []
+        success = False
+        
+        def verif_input(result, word):
+            for x, y in zip(result, word):
+                if x == '-':
+                    pass
+                elif x.lower() != y.lower():
+                    return False
+            return True
+        
+        while propcount < 6 or success:
+            if propcount == 0:
+                start_words = self.score_words(lang, first=True)
+                sorted_words = sorted([(w, start_words[w]) for w in start_words], key=itemgetter(1), reverse=True)
+                best_words = [w[0].upper() for w in sorted_words][:10]
+            else:
+                optimum = self.score_words_advanced(lang, tries, wrong)
+                sorted_words = sorted([(w, optimum[w]) for w in optimum], key=itemgetter(1), reverse=True)
+                best_words = [w[0].upper() for w in sorted_words][:3]
+            word = random.choice(best_words)
+                
+            prop_ok = False
+            while not prop_ok:
+                rtxt =  f"**Lettre dans le mot + __Bonne place__** = Lettre majuscule `{word[0].upper()}`" + "\n" + f"**Lettre dans le mot + __Mauvaise place__** = Lettre minuscule `{word[0].lower()}`" + "\n" + "**Lettre pas dans le mot** = Tiret ou point (`-` ou `.`)"
+                em = discord.Embed(title=f"**Wordle vs. {self.bot.user.name}** · Proposition #{propcount}",
+                                description=random.choice((f"Ma proposition est `{word}` !", "J'ai choisi `{choice}` !", "Partons pour `{choice}`.", "Allons pour `{choice}` !")))
+                em.add_field(name="Indiquer le résultat", value=rtxt, inline=False)
+                em.set_footer(text="» Notez ci-dessous le résultat avec le mot que j'ai donné :")
+                
+                msg = await ctx.reply(embed=em, mention_author=False)
+            
+                try:
+                    resultrep = await self.bot.wait_for('message', timeout=300, check=lambda m: m.author == user and len(m.content) == 5)
+                except asyncio.TimeoutError:
+                    await msg.delete(delay=5)
+                    return await ctx.reply(f"**Session expirée** · Avez-vous abandonné la partie ? Tant pis, à la prochaine !", mention_author=False)
+                
+                result = resultrep.content
+                result = result.replace('.', '-')
+                    
+                prop_ok = verif_input(result, word)
+                if not prop_ok:
+                    await ctx.reply(f"**Erreur** · Les lettres du résultat indiqué ne correspondent pas au mot que j'ai donné, réessayez.", mention_author=False)
+                
+            propcount += 1
+            
+            old_combi_try = ['-', '-', '-', '-', '-']
+            for t in tries:
+                i = 0
+                for l in t:
+                    if l.isupper() and old_combi_try[i] == '-':
+                        old_combi_try[i] = l
+                    i += 1
+            
+            wtry = [w for w in word.upper()]
+            wsave = []
+            i = 0
+            for r in result:
+                if r == '-':
+                    wsave.append('-')
+                    if wtry[i].lower() not in wrong:
+                        if wtry[i].upper() not in old_combi_try and wtry[i].upper() not in result.upper():
+                            wrong.append(wtry[i].lower())
+                elif r == wtry[i].upper():
+                    wsave.append(r)
+                elif r == wtry[i].lower():
+                    wsave.append(r)
+                i += 1
+                    
+            tries.append(wsave)
+        
+            if word == result:
+                success = True
+            else:
+                em = discord.Embed(title=f"**Wordle vs. {self.bot.user.name}** · Proposition #{propcount}",
+                                    description=random.choice(("Très bien, c'est noté !", "D'accord, je vois...", "Hmm, OK.", "Je vois, je vois...", "Intéressant, je m'attendais pas à ça.")))
+                em.set_footer(text="Je réfléchis...")
+                await ctx.send(embed=em)
+                await asyncio.sleep(random.randint(2, 4))
+        
+        if propcount == 6:
+            em = discord.Embed(title=f"**Wordle vs. {self.bot.user.name}** · Victoire (pour vous)",
+                               description="On dirait bien que j'ai perdu... Bien joué. Je ne me laisserai pas faire la prochaine fois !")
+        elif success:
+            em = discord.Embed(title=f"**Wordle vs. {self.bot.user.name}** · Défaite (pour vous)",
+                               description=f"J'ai gagné en **{propcount} propositions** ! Le mot était `{word}` ! Je suis vraiment trop fort. Bon allez, à la prochaine !")
+        await ctx.send(embed=em)
+        
+        
